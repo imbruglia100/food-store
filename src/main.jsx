5476a81ac8be45a24450017a561392c29f6f0e43
frank <imbruglia100@gmail.com>
2024-05-23 20:27:58 -0400

    phase 2 done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux'
 import './index.css';
+import configureStore from './store';
+import { populateProduce } from './store/produce'
+
+const store = configureStore()
+
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+  window.populateProduce = populateProduce;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
-
